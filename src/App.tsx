1cc40c2594621982bb10a384169ec9f512954438
desi.pineda.1 <desi.pineda.1@gmail.com>
2024-07-19 19:17:43 +0000

    Add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>Galaxy UI</h1>      
+      <h1>Galaxy UI</h1>
+      <nav>
+        <ul>
+          <li><Link to="/">Nova</Link></li>
+          <li><Link to="/milano">Milano</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' Component={Nova} />
+        <Route path='/milano' Component={Milano} />
+      </Routes>
     </>
   )
+}
+
+function Nova() {
+  return (<h2>Nova</h2>)
+}
+
+function Milano() {
+  return (<h2>Milano</h2>)
 }
 
 export default App
